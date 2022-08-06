7956f8d77e96ecf7d3705332b086fa9a4783d74f
HerculesCrato <46162819+HerculesCrato@users.noreply.github.com>
2022-08-06 13:48:26 -0300

    Add files via upload

---
+++ Matriz-Franca.py
@@ -0,0 +1,19 @@
+
+# Definição das bibliotecas a serem utilizadas
+
+import numpy 
+
+# Definição de constantes 
+
+N = 8 # Tamanho da matriz a ser usado nas linhas e colunas
+
+# crianção de variáveis, vetores e matrizes
+
+
+matriz = numpy.zeros((N,N)) 
+#cria uma matriz quadrada
+#de N linhas por N colunas preenchidas com zeros
+
+
+# print(matriz) # exibe a matriz criada
+
